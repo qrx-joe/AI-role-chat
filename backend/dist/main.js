3a10932d96a059fee2fd62b1284842c8b448b8c8
qrx-joe <1453626854@qq.com>
2026-01-25 13:37:14 +0800

    docs: Add OPTIMIZATION.md to document optimization guidelines.

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -8,6 +8,9 @@ const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
+    const express = require('express');
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ limit: '50mb', extended: true }));
     app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'), {
         prefix: '/',
     });
